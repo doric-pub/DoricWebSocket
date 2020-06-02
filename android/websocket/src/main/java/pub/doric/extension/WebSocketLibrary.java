5690e2e32a2ad65d8dee759fc41d806d083fed83
tracy <>
2020-06-02 10:19:37 +0800

    add doriccomponent annotation

--- android/websocket/src/main/java/pub/doric/extension/WebSocketLibrary.java
+++ android/websocket/src/main/java/pub/doric/extension/WebSocketLibrary.java
@@ -1,8 +1,10 @@
 package pub.doric.extension;
 
+import pub.doric.DoricComponent;
 import pub.doric.DoricLibrary;
 import pub.doric.DoricRegistry;
 
+@DoricComponent
 public class WebSocketLibrary extends DoricLibrary {
     @Override
     public void load(DoricRegistry registry) {
